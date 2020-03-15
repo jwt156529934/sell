1a430a44ee515699e7dbe0a4073357ac11b25599
root <123456>
2020-03-15 17:23:04 +0800

    项目基础环境搭建

---
+++ src/main/java/com/jwt/sell/SellApplication.java
@@ -0,0 +1,18 @@
+package com.jwt.sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuli涛涛
+ * @Title: SellApplication
+ * @ProjectName sell
+ * @Description: TODO
+ * @date 2020/3/1516:58
+ */
+@SpringBootApplication
+public class SellApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SellApplication.class,args);
+    }
+}
